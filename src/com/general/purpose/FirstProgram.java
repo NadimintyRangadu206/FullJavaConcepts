ab5b85c4d4fbceb101fe4def1368c49dac11fa89
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2023-08-20 13:46:39 +0530

    Solve the FindIntersection (CodeByte)

---
+++ src/com/general/purpose/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.general.purpose;
+
+public class FirstProgram {
+
+	
+	
+	
+	public static void main(String[] args) {
+		int no=10;
+//		FirstProgram fp=new FirstProgram();
+		System.out.println(no);
+	}
+
+}
