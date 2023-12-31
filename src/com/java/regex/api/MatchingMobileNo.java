7bcfb56fe37f85bdb1eb875bd3f555b35f48d9ca
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2023-12-31 18:22:19 +0530

    Regular Expression

---
+++ src/com/java/regex/api/MatchingMobileNo.java
@@ -0,0 +1,67 @@
+package com.java.regex.api;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchingMobileNo {
+
+	public static Pattern p = null;
+	public static Matcher m = null;
+
+	public static String phoneNumberMatching(String phoneNumber) {
+
+		p = Pattern.compile("\\b[6-9]?[0-9]{9}\\b");
+
+		m = p.matcher(phoneNumber);
+
+		if (m.matches()) {
+			return "Phone Number is Matches" + ":" + m.group();
+		} else {
+			return "Phone Number is doesnot Matchess";
+		}
+
+	}
+
+	public static String emailMatching(String email) {
+
+		p = Pattern.compile("\\b[A-Za-z0-9]+@[a-z]+\\.[a-z]{2,}\\b"); // \\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b
+
+		m = p.matcher(email);
+
+		if (m.matches()) {
+			return "EmailId Is Matches "+":"+m.group();
+		} else {
+			return "EmailId doesnot Matches";
+		}
+
+	}
+	
+	public static String userNameMatching(String userName) {
+		
+		p=Pattern.compile("^[A-Za-z]+(?:[-'\\\\s][A-Za-z]+)*$");
+		
+		m=p.matcher(userName);
+		
+		if(m.matches()) {
+			return "UerName is Matching"+":"+m.group();
+		}else {
+			return "UserName is doesnot Matching";
+		}
+	}
+
+	public static void main(String[] args) {
+
+		String str = "6281233658";
+
+		System.out.println(phoneNumberMatching(str));
+
+		String email = "nadimintyrangadu206@gmail.com";
+
+		System.out.println(emailMatching(email));
+		
+		String userName="Rangadu";
+		System.out.println(userNameMatching(userName));
+
+ 	}
+
+}
