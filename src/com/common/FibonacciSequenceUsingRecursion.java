d6bc2940127956089af12d12ba7a8c6b7cf2e91f
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2024-02-10 18:05:14 +0530

    Modifiaction

--- src/com/common/FibonacciSequenceUsingRecursion.java
+++ src/com/common/FibonacciSequenceUsingRecursion.java
@@ -1,26 +1,35 @@
-//package com.common;
-//
-//public class FibonacciSequenceUsingRecursion {
-//
-//	public void fib(int f1,int f2) {
-//		System.out.println(f1);
-//		System.out.println(f2);
-//		int f3=0;
-//		while() {
-//		f3=f1+f2;
-//		System.out.println(f3);
-//		f1=f2;
-//		f2=f3;
-//		fib(f1, f2);
-//		}
-//	}
-//	public static void main(String[] args) {
-//		
-//		
-//FibonacciSequenceUsingRecursion fb=new FibonacciSequenceUsingRecursion();
-//
-//fb.fib(0, 1);
-//
-//	}
-//
-//}
+package com.common;
+
+public class FibonacciSequenceUsingRecursion {
+
+	int no = 5;
+
+	public void fib(int f1, int f2) {
+
+		int f3 = 0;
+		while (no != 0) {
+
+			f3 = f1 + f2;
+			System.out.println(f3);
+			f1 = f2;
+			f2 = f3;
+			no--;
+			fib(f1, f2);
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		FibonacciSequenceUsingRecursion fb = new FibonacciSequenceUsingRecursion();
+
+		int f1 = 0;
+		int f2 = 1;
+		System.out.println(f1);
+		System.out.println(f2);
+		fb.fib(f1, f2);
+
+	}
+
+}
--- src/com/common/SortingArray.java
+++ src/com/common/SortingArray.java
@@ -1,4 +1,4 @@
-package com.common;
+   package com.common;
 
 public class SortingArray {
 
