fde5b81a30c4b340ba41a8e16d981977e800b99d
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2024-02-27 21:50:56 +0530

    Two Sum

---
+++ src/com/mostly/asked/coding/interview/questions/MergeIntervals.java
@@ -0,0 +1,13 @@
+package com.mostly.asked.coding.interview.questions;
+
+public class MergeIntervals {
+
+	public static void main(String[] args) {
+	
+		int arr[][]= {{1,3},{2,6},{8,10},{15,18}};
+		
+		 
+
+	}
+
+}
