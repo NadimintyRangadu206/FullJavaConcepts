53b0d198a7729e2751a57f37ad9fdd9e6d555160
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2024-03-06 15:25:04 +0530

    FindLengthOfLastWord

---
+++ src/com/mostly/asked/coding/interview/questions/AddBinary.java
@@ -0,0 +1,12 @@
+package com.mostly.asked.coding.interview.questions;
+
+public class AddBinary {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+}
---
+++ src/com/mostly/asked/coding/interview/questions/ZigzagConversion.java
@@ -0,0 +1,30 @@
+package com.mostly.asked.coding.interview.questions;
+
+
+
+public class ZigzagConversion {
+
+	
+	
+	public  static String convert(String s, int numRows) {
+		
+	
+
+        char ch[]=s.toCharArray();
+
+        char zig[][]=new char[numRows][ch.length/2];
+
+       
+        
+        
+        return null;
+
+    }
+	
+	public static void main(String[] args) {
+		
+		String s="PAYPALISHIRING";
+		int numRows=3;
+		convert(s,numRows );
+	}
+}
