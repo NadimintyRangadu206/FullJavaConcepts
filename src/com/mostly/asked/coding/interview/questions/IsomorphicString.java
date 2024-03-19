496261106560c6297b96f65df657da02b0d04752
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2024-03-19 12:16:26 +0530

    Update Isomorphic Strings

--- src/com/mostly/asked/coding/interview/questions/IsomorphicString.java
+++ src/com/mostly/asked/coding/interview/questions/IsomorphicString.java
@@ -1,5 +1,7 @@
 package com.mostly.asked.coding.interview.questions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,81 +9,63 @@ import java.util.Map;
  * 
  * @author nadim
  * 
- * Given two strings s and t, determine if they are isomorphic.
-
-Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-
-All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
- 
-
-Example 1:
-
-Input: s = "egg", t = "add"
-Output: true
-Example 2:
-
-Input: s = "foo", t = "bar"
-Output: false
-Example 3:
-
-Input: s = "paper", t = "title"
-Output: true
- 
-
-Constraints:
-
-1 <= s.length <= 5 * 104
-t.length == s.length
-s and t consist of any valid ascii character.
+ *         Given two strings s and t, determine if they are isomorphic.
+ * 
+ *         Two strings s and t are isomorphic if the characters in s can be
+ *         replaced to get t.
+ * 
+ *         All occurrences of a character must be replaced with another
+ *         character while preserving the order of characters. No two characters
+ *         may map to the same character, but a character may map to itself.
+ * 
+ * 
+ * 
+ *         Example 1:
+ * 
+ *         Input: s = "egg", t = "add" Output: true Example 2:
+ * 
+ *         Input: s = "foo", t = "bar" Output: false Example 3:
+ * 
+ *         Input: s = "paper", t = "title" Output: true
+ * 
+ * 
+ *         Constraints:
+ * 
+ *         1 <= s.length <= 5 * 104 t.length == s.length s and t consist of any
+ *         valid ascii character.
  *
  */
 
 public class IsomorphicString {
 
-	static boolean isomorphic(String s, String t) {
+	public static boolean isIsomorphic(String s, String t) {
 
 		if (s.length() != t.length()) {
+
 			return false;
 		}
 
-		if (s.length() == 0 && t.length() == 0) {
+		if (s.isEmpty() && t.isEmpty()) {
 			return true;
 		}
 
-		HashMap<Character, Character> map = new HashMap<Character, Character>();
+		int map1[] = new int[200];
+		int map2[] = new int[200];
+
+		if (s.length() != t.length())
+			return false;
 
 		for (int i = 0; i < s.length(); i++) {
-
-			char c1 = s.charAt(i);
-			char c2 = t.charAt(i);
-
-			Character ch = getKey(map, c2);
-
-			if (ch != null && ch != c1) {
+			if (map1[s.charAt(i)] != map2[t.charAt(i)]) {
 				return false;
-			} else if (map.containsKey(c1)) {
-				if (c2 != map.get(c1)) {
-					return false;
-				} else {
-					map.put(c1, c2);
-				}
+
 			}
+
+			map1[s.charAt(i)] = i + 1;
+			map2[t.charAt(i)] = i + 1;
 		}
 		return true;
 
-	}
-
-	public static Character getKey(HashMap<Character, Character> map, Character target) {
-
-		for (Map.Entry entry : map.entrySet()) {
-
-			if (entry.getValue().equals(target)) {
-				return (Character) entry.getKey();
-			}
-		}
-
-		return null;
 	}
 
 	public static void main(String[] args) {
@@ -114,7 +98,7 @@ public class IsomorphicString {
 			System.out.println("Not Isomorphic");
 		}
 
-		System.out.println(isomorphic(s, t));
+		System.out.println(isIsomorphic(s, t));
 
 	}
 
