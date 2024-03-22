cbebcbc5f3f95d83a8558bbe34de2b716c53a623
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2024-03-22 17:25:14 +0530

    update lagest Valid Parenthesis

--- src/com/mostly/asked/coding/interview/questions/LogestValidParenthese.java
+++ src/com/mostly/asked/coding/interview/questions/LogestValidParenthese.java
@@ -1,45 +1,88 @@
 package com.mostly.asked.coding.interview.questions;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+/**
+ * Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses 
+substring
+.
 
+ 
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+Example 3:
+
+Input: s = ""
+Output: 0
+ * @author nadim
+ *
+ */
 public class LogestValidParenthese {
+
 	public static int longestValidParentheses(String s) {
 
-		char[] ch = s.toCharArray();
+		int left = 0;
+		int right = 0;
+		int max = 0;
 
-		Stack<Character> sk = new Stack<>();
-		List<Character> list = new ArrayList<>();
+		for (int i = 0; i < s.length(); i++) {
 
-		for (char c : ch) {
+			if (s.charAt(i) == '(') {
 
-			switch (c) {
-
-			case '(':
-				sk.push(c);
-				break;
-
-			case ')':
-
-				if(sk.isEmpty()) {
-					continue;
-				}
-				 else if(sk.pop() == '(') {
-					list.add('(');
-					list.add(')');
-				}
-				break;
-
+				left++;
+			} else {
+				right++;
 			}
+
+			if (left == right) {
+
+				max = Math.max(max, right * 2);
+			}
+
+			if (right > left) {
+				left = 0;
+				right = 0;
+			}
+
 		}
 
-		return list.size();
+		left = 0;
+		right = 0;
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+
+			if (s.charAt(i) == '(') {
+
+				left++;
+			} else {
+				right++;
+			}
+
+			if (left == right) {
+
+				max = Math.max(max, right * 2);
+			}
+
+			if (left > right) {
+				left = 0;
+				right = 0;
+			}
+
+		}
+
+		return max;
 	}
 
 	public static void main(String[] args) {
 
-		String s = "()(()";
+		String s = ")()()(()()()";
 
 		System.out.println(longestValidParentheses(s));
 
